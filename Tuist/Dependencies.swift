7a1b6890c2cb0b81e532d2f85572a872b17c96d1
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-09-19 12:22:51 +0900

    ➕ [Add] SnapKit 의존성 추가
    * Tuist로 외부 패키지의 Dependency를 관리합니다.
    * SnapKit 의존성을 추가했습니다.
    * App프로젝트의 의존성에 SnapKit을 추가했습니다

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,6 +9,7 @@ import ProjectDescription
 
 let dependencies = Dependencies(
     swiftPackageManager: [
+        .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1"))
     ],
     platforms: [.iOS]
 )
