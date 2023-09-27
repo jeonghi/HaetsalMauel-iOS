0ecc9915977102e28207f5ff8efac3b51ea33dc2
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-09-27 12:51:26 +0900

    [Add] MainTab 화면 추가
    - 메인 탭 화면을 추가하였습니다.

--- Projects/App/Sources/Presentation/MainTabScene/MainTab/MainTabView.swift
+++ Projects/App/Sources/Presentation/MainTabScene/MainTab/MainTabView.swift
@@ -9,8 +9,11 @@
 import SwiftUI
 
 struct MainTab: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            HomeView()
+        }
     }
 }
 
