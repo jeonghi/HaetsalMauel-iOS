fb06598638150059c8171dbdc919e1d1b03842e0
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-09-27 16:23:35 +0900

    [Add] 설정 탭 화면, 리듀서 정의

--- Projects/App/Sources/Presentation/SettingScene/Setting/SettingView.swift
+++ Projects/App/Sources/Presentation/SettingScene/Setting/SettingView.swift
@@ -7,15 +7,43 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct SettingView: View {
+
+    typealias Core = Setting
+    typealias State = Core.State
+    typealias Action = Core.Action
+    typealias Route = Core.Route
+
+    private let store: StoreOf<Core>
+
+    struct ViewState: Equatable {
+        init(state: State){
+            
+        }
+    }
+    
+    init(store: StoreOf<Core>){
+        self.store = store
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // 여기에 작성
+        VStack(spacing: 0){
+            Text("설정화면")
+        }
+        .navigationBarTitle("설정")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingView()
+        let store = Store(initialState: Setting.State()){
+            Setting()
+        }
+        SettingView(store: store)
     }
 }
