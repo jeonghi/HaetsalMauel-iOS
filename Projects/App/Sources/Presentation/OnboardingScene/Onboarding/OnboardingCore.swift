4fdc2281d50cf4731489912cf9d7d4815555f28f
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-09-27 16:25:30 +0900

    [Add] 온보딩 화면 리듀서 추가
    * 온보딩 화면의 리듀서를 정의했어요
    * 액션에 둘러보기 버튼 탭 액션을 추가했어요.

--- Projects/App/Sources/Presentation/OnboardingScene/Onboarding/OnboardingCore.swift
+++ Projects/App/Sources/Presentation/OnboardingScene/Onboarding/OnboardingCore.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2023 com.eum. All rights reserved.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct Onboarding: Reducer {
+  
+  struct State {
+    var route: Route? = nil
+  }
+  
+  enum Route {
+    case login
+    case logout
+  }
+  
+  enum SocialLoginAdvisor {
+    case kakao // 카카오
+    case google // 구글
+    case apple // 애플
+  }
+  
+  enum Action {
+    case onAppear
+    case onDisappear
+    case setRoute(Route)
+    
+    case skipButtonTapped
+  }
+  
+  func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    switch action {
+    case .onAppear:
+      return .none
+    case .onDisappear:
+      return .none
+    case .setRoute(let selectedRoute):
+      state.route = selectedRoute
+      return .none
+    case .skipButtonTapped:
+      return .none
+    }
+  }
+}
