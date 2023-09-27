df3e607e5868b3fa7e407310ec0a016919874d25
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-09-27 10:24:03 +0900

    ➕ [Add] 디자인 시스템 프로젝트에 Preview 코드 추가
    * UIButton 에 Preview기능을 추가하였습니다.
    * UIViewController에 Preview 기능을 추가하였습니다

---
+++ Projects/DesignSystem/Sources/Extension/UIViewController+Preview.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController+Extensions.swift
+//  Eum
+//
+//  Created by JH Park on 2023/09/13.
+//  Copyright © 2023 tuist.io. All rights reserved.
+//
+import UIKit
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
+
