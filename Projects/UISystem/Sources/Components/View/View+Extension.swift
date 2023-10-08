9d4000a7c46afbaa3fdca5a1889670194e715e78
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-10-08 14:11:37 +0900

    [Add] View 정렬 익스텐션 함수 추가

--- Projects/UISystem/Sources/Components/View/View+Extension.swift
+++ Projects/UISystem/Sources/Components/View/View+Extension.swift
@@ -8,6 +8,38 @@
 import SwiftUI
 
 public extension View {
+  /// 수평 정렬: leading
+  func hLeading() -> some View {
+    self.frame(maxWidth: .infinity, alignment: .leading)
+  }
+  
+  /// 수평 정렬: trailing
+  func hTrailing() -> some View {
+    self.frame(maxWidth: .infinity, alignment: .trailing)
+  }
+  
+  /// 수평 정렬: center
+  func hCenter() -> some View {
+    self.frame(maxWidth: .infinity, alignment: .center)
+  }
+  
+  /// 수직 정렬: top
+  func vTop() -> some View {
+    self.frame(maxHeight: .infinity, alignment: .top)
+  }
+  
+  /// 수직 정렬: bottom
+  func vBottom() -> some View {
+    self.frame(maxHeight: .infinity, alignment: .bottom)
+  }
+  
+  /// 수직 정렬: center
+  func vCenter() -> some View {
+    self.frame(maxHeight: .infinity, alignment: .center)
+  }
+}
+
+public extension View {
   /// 특정 모서리 cornerRadius
   func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
     clipShape(RoundedCorner(radius: radius, corners: corners))
