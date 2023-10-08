08a6e0465b04245821276863c8e7a9e118597bd2
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-10-08 14:04:51 +0900

    [Add] 서버 base url 설정파일 추가

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -2,242 +2,255 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 public extension Project {
+  
+  static let organizationName = "kr.k-eum"
+  static let targetVersion = "15.0"
+  
+  
+  static func makeAppModule(
+    name: String,
+    platform: Platform = .iOS,
+    product: Product,
+    organizationName: String = organizationName,
+    packages: [Package] = [],
+    deploymentTarget: DeploymentTarget? = .iOS(targetVersion: targetVersion, devices: [.iphone]),
+    dependencies: [TargetDependency] = [],
+    sources: SourceFilesList = ["Sources/**"],
+    resources: ResourceFileElements? = nil,
+    infoPlist: InfoPlist = .default,
+    entitlements: Path? = nil
+  ) -> Project {
     
-    static func makeAppModule(
-        name: String,
-        platform: Platform = .iOS,
-        product: Product,
-        organizationName: String = "com.eum",
-        packages: [Package] = [],
-        deploymentTarget: DeploymentTarget? = .iOS(targetVersion: "15.0", devices: [.iphone]),
-        dependencies: [TargetDependency] = [],
-        sources: SourceFilesList = ["Sources/**"],
-        resources: ResourceFileElements? = nil,
-        infoPlist: InfoPlist = .default,
-        entitlements: Path? = nil
-    ) -> Project {
-        let settings: Settings = .settings(defaultSettings: .recommended)
-        
-        let appTarget = Target(
-            name: name,
-            platform: platform,
-            product: product,
-            bundleId: "\(organizationName).\(name)",
-            deploymentTarget: deploymentTarget,
-            infoPlist: infoPlist,
-            sources: sources,
-            resources: resources,
-            entitlements: entitlements,
-            dependencies: dependencies
-        )
-        
-        let testTarget = Target(
-            name: "\(name)Tests",
-            platform: platform,
-            product: .unitTests,
-            bundleId: "\(organizationName).\(name)Tests",
-            deploymentTarget: deploymentTarget,
-            infoPlist: .default,
-            sources: ["Tests/**"],
-            entitlements: entitlements,
-            dependencies: [.target(name: name)]
-        )
-        
-        let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
-        
-        let targets: [Target] = [appTarget, testTarget]
-        
-        return Project(
-            name: name,
-            organizationName: organizationName,
-            packages: packages,
-            settings: settings,
-            targets: targets,
-            schemes: schemes
-        )
-    }
+    let settings: Settings = .settings(
+//      base: fireBaseAnalyticsSetting,
+      configurations: [
+        .debug(name: .debug, xcconfig: .relativeToManifest("debug-config.xcconfig")),
+        .release(name: .release, xcconfig: .relativeToManifest("release-config.xcconfig"))
+      ],
+      defaultSettings: .recommended
+    )
     
-    static func makeDesignSystemFoundationModule(
-        name: String,
-        platform: Platform = .iOS,
-        product: Product,
-        organizationName: String = "com.eum",
-        packages: [Package] = [],
-        deploymentTarget: DeploymentTarget? = .iOS(targetVersion: "15.0", devices: [.iphone]),
-        dependencies: [TargetDependency] = [],
-        sources: SourceFilesList = ["Sources/**"],
-        resources: ResourceFileElements? = nil,
-        infoPlist: InfoPlist = .default
-    ) -> Project {
-        let settings: Settings = .settings(
-            configurations: [
-                .debug(name: .debug),
-                .release(name: .release)
-            ], defaultSettings: .recommended)
-        
-        let appTarget = Target(
-            name: name,
-            platform: platform,
-            product: product,
-            bundleId: "\(organizationName).\(name)",
-            deploymentTarget: deploymentTarget,
-            infoPlist: infoPlist,
-            sources: sources,
-            resources: resources,
-            dependencies: dependencies
-        )
-        
-        let testTarget = Target(
-            name: "\(name)Tests",
-            platform: platform,
-            product: .unitTests,
-            bundleId: "\(organizationName).\(name)Tests",
-            deploymentTarget: deploymentTarget,
-            infoPlist: .default,
-            sources: ["Tests/**"],
-            dependencies: [.target(name: name)]
-        )
-        
-        let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
-        
-        let targets: [Target] = [appTarget, testTarget]
-        
-        return Project(
-            name: name,
-            organizationName: organizationName,
-            packages: packages,
-            settings: settings,
-            targets: targets,
-            schemes: schemes
-        )
-    }
+    let appTarget = Target(
+      name: name,
+      platform: platform,
+      product: product,
+      bundleId: "\(organizationName).\(name)",
+      deploymentTarget: deploymentTarget,
+      infoPlist: infoPlist,
+      sources: sources,
+      resources: resources,
+      entitlements: entitlements,
+      dependencies: dependencies
+    )
     
-    static func makeUISystemModule(
-        name: String,
-        platform: Platform = .iOS,
-        product: Product,
-        organizationName: String = "com.eum",
-        packages: [Package] = [],
-        deploymentTarget: DeploymentTarget? = .iOS(targetVersion: "15.0", devices: [.iphone]),
-        dependencies: [TargetDependency] = [],
-        sources: SourceFilesList = ["Sources/**"],
-        resources: ResourceFileElements? = nil,
-        infoPlist: InfoPlist = .default
-    ) -> Project {
-        let settings: Settings = .settings(
-            configurations: [
-                .debug(name: .debug),
-                .release(name: .release)
-            ], defaultSettings: .recommended)
-        
-        let appTarget = Target(
-            name: name,
-            platform: platform,
-            product: product,
-            bundleId: "\(organizationName).\(name)",
-            deploymentTarget: deploymentTarget,
-            infoPlist: infoPlist,
-            sources: sources,
-            resources: resources,
-            dependencies: dependencies
-        )
-        
-        let testTarget = Target(
-            name: "\(name)Tests",
-            platform: platform,
-            product: .unitTests,
-            bundleId: "\(organizationName).\(name)Tests",
-            deploymentTarget: deploymentTarget,
-            infoPlist: .default,
-            sources: ["Tests/**"],
-            dependencies: [.target(name: name)]
-        )
-        
-        let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
-        
-        let targets: [Target] = [appTarget, testTarget]
-        
-        return Project(
-            name: name,
-            organizationName: organizationName,
-            packages: packages,
-            settings: settings,
-            targets: targets,
-            schemes: schemes
-        )
-    }
+    let testTarget = Target(
+      name: "\(name)Tests",
+      platform: platform,
+      product: .unitTests,
+      bundleId: "\(organizationName).\(name)Tests",
+      deploymentTarget: deploymentTarget,
+      infoPlist: .default,
+      sources: ["Tests/**"],
+      entitlements: entitlements,
+      dependencies: [.target(name: name)]
+    )
     
-    static func makeNetworkModule(
-        name: String,
-        platform: Platform = .iOS,
-        product: Product,
-        organizationName: String = "com.eum",
-        packages: [Package] = [],
-        deploymentTarget: DeploymentTarget? = .iOS(targetVersion: "15.0", devices: [.iphone]),
-        dependencies: [TargetDependency] = [],
-        sources: SourceFilesList = ["Sources/**"],
-        resources: ResourceFileElements? = nil,
-        infoPlist: InfoPlist = .default
-    ) -> Project {
-        let settings: Settings = .settings(
-            configurations: [
-                .debug(name: .debug),
-                .release(name: .release)
-            ], defaultSettings: .recommended)
-        
-        let appTarget = Target(
-            name: name,
-            platform: platform,
-            product: product,
-            bundleId: "\(organizationName).\(name)",
-            deploymentTarget: deploymentTarget,
-            infoPlist: infoPlist,
-            sources: sources,
-            resources: resources,
-            dependencies: dependencies
-        )
-        
-        let testTarget = Target(
-            name: "\(name)Tests",
-            platform: platform,
-            product: .unitTests,
-            bundleId: "\(organizationName).\(name)Tests",
-            deploymentTarget: deploymentTarget,
-            infoPlist: .default,
-            sources: ["Tests/**"],
-            dependencies: [.target(name: name)]
-        )
-        
-        let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
-        
-        let targets: [Target] = [appTarget, testTarget]
-        
-        return Project(
-            name: name,
-            organizationName: organizationName,
-            packages: packages,
-            settings: settings,
-            targets: targets,
-            schemes: schemes
-        )
-    }
+    let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
+    
+    let targets: [Target] = [appTarget, testTarget]
+    
+    return Project(
+      name: name,
+      organizationName: organizationName,
+      packages: packages,
+      settings: settings,
+      targets: targets,
+      schemes: schemes
+    )
+  }
+  
+  static func makeDesignSystemFoundationModule(
+    name: String,
+    platform: Platform = .iOS,
+    product: Product,
+    organizationName: String = organizationName,
+    packages: [Package] = [],
+    deploymentTarget: DeploymentTarget? = .iOS(targetVersion: targetVersion, devices: [.iphone]),
+    dependencies: [TargetDependency] = [],
+    sources: SourceFilesList = ["Sources/**"],
+    resources: ResourceFileElements? = nil,
+    infoPlist: InfoPlist = .default
+  ) -> Project {
+    let settings: Settings = .settings(
+      configurations: [
+        .debug(name: .debug),
+        .release(name: .release)
+      ],
+      defaultSettings: .recommended)
+    
+    let appTarget = Target(
+      name: name,
+      platform: platform,
+      product: product,
+      bundleId: "\(organizationName).\(name)",
+      deploymentTarget: deploymentTarget,
+      infoPlist: infoPlist,
+      sources: sources,
+      resources: resources,
+      dependencies: dependencies
+    )
+    
+    let testTarget = Target(
+      name: "\(name)Tests",
+      platform: platform,
+      product: .unitTests,
+      bundleId: "\(organizationName).\(name)Tests",
+      deploymentTarget: deploymentTarget,
+      infoPlist: .default,
+      sources: ["Tests/**"],
+      dependencies: [.target(name: name)]
+    )
+    
+    let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
+    
+    let targets: [Target] = [appTarget, testTarget]
+    
+    return Project(
+      name: name,
+      organizationName: organizationName,
+      packages: packages,
+      settings: settings,
+      targets: targets,
+      schemes: schemes
+    )
+  }
+  
+  static func makeUISystemModule(
+    name: String,
+    platform: Platform = .iOS,
+    product: Product,
+    organizationName: String = organizationName,
+    packages: [Package] = [],
+    deploymentTarget: DeploymentTarget? = .iOS(targetVersion: targetVersion, devices: [.iphone]),
+    dependencies: [TargetDependency] = [],
+    sources: SourceFilesList = ["Sources/**"],
+    resources: ResourceFileElements? = nil,
+    infoPlist: InfoPlist = .default
+  ) -> Project {
+    let settings: Settings = .settings(
+      configurations: [
+        .debug(name: .debug),
+        .release(name: .release)
+      ], defaultSettings: .recommended)
+    
+    let appTarget = Target(
+      name: name,
+      platform: platform,
+      product: product,
+      bundleId: "\(organizationName).\(name)",
+      deploymentTarget: deploymentTarget,
+      infoPlist: infoPlist,
+      sources: sources,
+      resources: resources,
+      dependencies: dependencies
+    )
+    
+    let testTarget = Target(
+      name: "\(name)Tests",
+      platform: platform,
+      product: .unitTests,
+      bundleId: "\(organizationName).\(name)Tests",
+      deploymentTarget: deploymentTarget,
+      infoPlist: .default,
+      sources: ["Tests/**"],
+      dependencies: [.target(name: name)]
+    )
+    
+    let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
+    
+    let targets: [Target] = [appTarget, testTarget]
+    
+    return Project(
+      name: name,
+      organizationName: organizationName,
+      packages: packages,
+      settings: settings,
+      targets: targets,
+      schemes: schemes
+    )
+  }
+  
+  static func makeNetworkModule(
+    name: String,
+    platform: Platform = .iOS,
+    product: Product,
+    organizationName: String = organizationName,
+    packages: [Package] = [],
+    deploymentTarget: DeploymentTarget? = .iOS(targetVersion: targetVersion, devices: [.iphone]),
+    dependencies: [TargetDependency] = [],
+    sources: SourceFilesList = ["Sources/**"],
+    resources: ResourceFileElements? = nil,
+    infoPlist: InfoPlist = .default
+  ) -> Project {
+    let settings: Settings = .settings(
+      configurations: [
+        .debug(name: .debug),
+        .release(name: .release)
+      ], defaultSettings: .recommended)
+    
+    let appTarget = Target(
+      name: name,
+      platform: platform,
+      product: product,
+      bundleId: "\(organizationName).\(name)",
+      deploymentTarget: deploymentTarget,
+      infoPlist: infoPlist,
+      sources: sources,
+      resources: resources,
+      dependencies: dependencies
+    )
+    
+    let testTarget = Target(
+      name: "\(name)Tests",
+      platform: platform,
+      product: .unitTests,
+      bundleId: "\(organizationName).\(name)Tests",
+      deploymentTarget: deploymentTarget,
+      infoPlist: .default,
+      sources: ["Tests/**"],
+      dependencies: [.target(name: name)]
+    )
+    
+    let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
+    
+    let targets: [Target] = [appTarget, testTarget]
+    
+    return Project(
+      name: name,
+      organizationName: organizationName,
+      packages: packages,
+      settings: settings,
+      targets: targets,
+      schemes: schemes
+    )
+  }
 }
 
 extension Scheme {
-    static func makeScheme(target: ConfigurationName, name: String) -> Scheme {
-        return Scheme(
-            name: name,
-            shared: true,
-            buildAction: .buildAction(targets: ["\(name)"]),
-            testAction: .targets(
-                ["\(name)Tests"],
-                configuration: target,
-                options: .options(coverage: true, codeCoverageTargets: ["\(name)"])
-            ),
-            runAction: .runAction(configuration: target),
-            archiveAction: .archiveAction(configuration: target),
-            profileAction: .profileAction(configuration: target),
-            analyzeAction: .analyzeAction(configuration: target)
-        )
-    }
+  static func makeScheme(target: ConfigurationName, name: String) -> Scheme {
+    return Scheme(
+      name: name,
+      shared: true,
+      buildAction: .buildAction(targets: ["\(name)"]),
+      testAction: .targets(
+        ["\(name)Tests"],
+        configuration: target,
+        options: .options(coverage: true, codeCoverageTargets: ["\(name)"])
+      ),
+      runAction: .runAction(configuration: target),
+      archiveAction: .archiveAction(configuration: target),
+      profileAction: .profileAction(configuration: target),
+      analyzeAction: .analyzeAction(configuration: target)
+    )
+  }
 }
