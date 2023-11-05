bb78cb65cfa4c7782b8be26c2fd41dbd2d92657d
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-11-05 21:48:45 +0900

    [Add] 카테고리 모델 추가

---
+++ Projects/App/Sources/Model/CommunityCategory.swift
@@ -0,0 +1,19 @@
+//
+//  CommunityCategory.swift
+//  App
+//
+//  Created by JH Park on 2023/10/02.
+//  Copyright © 2023 com.eum. All rights reserved.
+//
+
+enum CommunityCategory: String, CaseIterable {
+  case 이동
+  case 심부름
+  case 교육
+  case 청소
+  case 돌봄
+  case 수리
+  case 물건
+  case 의견
+  case 기타
+}
