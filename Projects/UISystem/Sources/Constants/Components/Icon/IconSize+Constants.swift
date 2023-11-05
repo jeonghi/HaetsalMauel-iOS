00b96b711d131e552e9a408a74326dead87050a7
Park Jeong Hwan <jpark0902@kookmin.ac.kr>
2023-11-05 22:24:08 +0900

    [Add] Icon 디자인 에셋 추가

---
+++ Projects/UISystem/Sources/Constants/Components/Icon/IconSize+Constants.swift
@@ -0,0 +1,15 @@
+//
+//  IconSize+Constants.swift
+//  UISystem
+//
+//  Created by JH Park on 2023/09/28.
+//  Copyright © 2023 com.eum. All rights reserved.
+//
+import DesignSystemFoundation
+
+extension IconSize {
+  static var xxLarge = IconSize(width: 60, height: 60)
+  static var large = IconSize(width: 24, height: 24)
+  static var medium = IconSize(width: 16, height: 16)
+  static var small = IconSize(width: 12, height: 12)
+}
