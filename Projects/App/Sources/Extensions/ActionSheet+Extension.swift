3ab47e25f2571a23b442db8ce3caced81dbcf330
jeonghi <jpark0902@kookmin.ac.kr>
2023-11-17 00:03:27 +0900

    [Add] 액션 시트 버튼 커스텀 정의

--- Projects/App/Sources/Extensions/ActionSheet+Extension.swift
+++ Projects/App/Sources/Extensions/ActionSheet+Extension.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2023 kr.k-eum. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import UISystem
+import DesignSystemFoundation
+
+extension ActionSheet.Button {
+    static func defaultButton(title: String, font: Font = .headerB, action: @escaping () -> Void) -> ActionSheet.Button {
+      .default(Text(title).font(font).foregroundColor(Color(.black)), action: action)
+    }
+
+    static func destructiveButton(title: String, font: Font = .headerB, action: @escaping () -> Void) -> ActionSheet.Button {
+      .destructive(Text(title).font(font).foregroundColor(Color(.black)), action: action)
+    }
+
+    static func cancelButton(title: String = "취소", font: Font = .headerB) -> ActionSheet.Button {
+        .cancel(Text(title).font(font))
+    }
+}
