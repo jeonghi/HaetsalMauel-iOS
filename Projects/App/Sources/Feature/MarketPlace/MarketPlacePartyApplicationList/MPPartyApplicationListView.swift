31b3aeed9cc64bb567b39d11525fd5f393c9bdc4
jeonghi <jpark0902@kookmin.ac.kr>
2023-11-26 20:35:25 +0900

    [Add] 햇터 활동 신청 목록 리스트 화면 추가

---
+++ Projects/App/Sources/Feature/MarketPlace/MarketPlacePartyApplicationList/MPPartyApplicationListView.swift
@@ -0,0 +1,19 @@
+//
+//  MPPartyApplicationListView.swift
+//  App
+//
+//  Created by 쩡화니 on 11/21/23.
+//  Copyright © 2023 kr.k-eum. All rights reserved.
+//
+
+import SwiftUI
+
+struct MPPartyApplicationListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MPPartyApplicationListView()
+}
