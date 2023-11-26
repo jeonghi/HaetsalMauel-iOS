ab289799088642b485c4a9e433b4cfcd9cd24d82
jeonghi <jpark0902@kookmin.ac.kr>
2023-11-27 06:44:49 +0900

    [Add] 토큰 매니저 정의

--- Projects/EumAuth/Sources/Manager/TokenManager.swift
+++ Projects/EumAuth/Sources/Manager/TokenManager.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import KeychainAccess
+
+public protocol TokenManagable {
+  /// 토큰을 저장
+  func setToken(_ token: OAuthToken?)
+  /// 저장된 토큰 가져오기
+  func getToken() -> OAuthToken?
+  /// 저장된 토큰 삭제하기
+  func deleteToken()
+}
+
+final public class TokenManager: TokenManagable {
+  
+  public static let shared = TokenManager()
+  
+  private let OAuthTokenKey = "kr.k-eum.auth.oauth_token"
+  
+  var token: OAuthToken?
+  
+  private init() {
+    self.token = Properties.loadCodable(key: OAuthTokenKey)
+  }
+  
+  public func setToken(_ token: OAuthToken?) {
+    Properties.saveCodable(key: OAuthTokenKey, data: token)
+  }
+  
+  public func getToken() -> OAuthToken? {
+    return self.token
+  }
+  
+  public func deleteToken() {
+    Properties.delete(OAuthTokenKey)
+    self.token = nil
+  }
+}
