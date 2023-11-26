a903709369699f6bc8ad5083c3303e1fd5fbc6ea
jeonghi <jpark0902@kookmin.ac.kr>
2023-11-27 05:26:23 +0900

    [Add] 상수 구조체 추가

---
+++ Projects/EumNetwork/Sources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  EumNetwork
+//
+//  Created by 쩡화니 on 11/26/23.
+//  Copyright © 2023 kr.k-eum. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+  static let baseUrl = "http://sunrise.k-eum.kr:8080"
+}
