9d8c62cdf1a43a9a8b496df453056d060e5bd424
jeonghi <jpark0902@kookmin.ac.kr>
2023-12-06 20:41:30 +0900

    [Add] 유저 프로필 생성 화면 추가

--- Projects/App/Sources/Feature/UserProfile/UPCreate/UPCreateCore.swift
+++ Projects/App/Sources/Feature/UserProfile/UPCreate/UPCreateCore.swift
@@ -8,25 +8,27 @@
 
 import Foundation
 import ComposableArchitecture
+import EumNetwork
+import Combine
 
 struct UPCreate: Reducer {
   
   typealias Category = CharacterCategory
   
-  struct State: Equatable {
+  struct State {
     var nickName: String = ""// 닉네임
-    var location: String = ""// 지역
     var selectedCategory: Category? {
       CCSelectionState.selectedCategory
     }
     var CCSelectionState: CCSelection.State = .init()
+    var regionSelectionState: RegionSelection.State = .init()
     
     var isAllInfoFilled: Bool {
-      true
+      CCSelectionState.isSelectedAnyOne && regionSelectionState.allRegionIsSelected && !nickName.isEmpty
     }
   }
   
-  enum Action: Equatable {
+  enum Action {
     /// Life cycle
     case onAppear
     case onDisappear
@@ -35,6 +37,11 @@ struct UPCreate: Reducer {
     case tappedNextButton
     
     case CCSelectionAction(CCSelection.Action)
+    case regionSelectionAction(RegionSelection.Action)
+    
+    /// Network
+    case requestCreateProfile
+    case requestCreateProfileResponse(Result<ProfileEntity.Response?, HTTPError>)
   }
   
   var body: some ReducerOf<Self> {
@@ -52,9 +59,44 @@ struct UPCreate: Reducer {
         return .none
         
       case .tappedNextButton:
-        return .none
+        return .send(.requestCreateProfile)
         
       case .CCSelectionAction(let act):
+        return .none
+      case .regionSelectionAction(let act):
+        return .none
+      case .requestCreateProfile:
+        
+        guard let cat = state.selectedCategory, let type = state.selectedCategory, let regionId = state.regionSelectionState.selectedDONG?.regionId else {
+          logger.log(.warning, "카테고리 정보 없음")
+          return .none
+        }
+        
+        var avatarName: ProfileEntity.AvatarName {
+          switch cat {
+          case .중장년:
+            return .middle
+          case .청년:
+            return .young
+          case .노년:
+            return .old
+          case .청소년:
+            return .youth
+          }
+        }
+        
+        let request = ProfileEntity.Request(avatarName: avatarName , introduction: "", nickname: state.nickName, regionId: regionId)
+        return .publisher {
+          profileService.createProfile(request)
+            .receive(on: mainQueue)
+            .map{Action.requestCreateProfileResponse(.success($0))}
+            .catch{Just(Action.requestCreateProfileResponse(.failure($0)))}
+        }
+      case .requestCreateProfileResponse(.success(let res)):
+        logger.log(.debug, res)
+        return .none
+      case .requestCreateProfileResponse(.failure(let error)):
+        logger.log(.error, error)
         return .none
       }
     }
@@ -62,6 +104,14 @@ struct UPCreate: Reducer {
     Scope(state: \.CCSelectionState, action: /Action.CCSelectionAction){
       CCSelection()
     }
+    
+    Scope(state: \.regionSelectionState, action: /Action.regionSelectionAction){
+      RegionSelection()
+    }
   }
+  
+  @Dependency(\.appService.profileService) var profileService
+  @Dependency(\.mainQueue) var mainQueue
+  @Dependency(\.logger) var logger
 }
 
